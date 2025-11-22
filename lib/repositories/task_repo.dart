c9c134dadeab7f55c63141b3026f62199506a0a3
exspiravit <exspiravit@debian.debian.com>
2025-11-21 22:25:13 -0300

    mudando o salvamento de dados de shared_preferences para o path_profider (mais seguro)

--- lib/repositories/task_repo.dart
+++ lib/repositories/task_repo.dart
@@ -1,28 +1,71 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tarefas/task_model.dart';
 
 class TaskRepo{
 
-  TaskRepo(){
-    SharedPreferences.getInstance().then(
-      (value)=> sharedPreferences = value
-    );
-  }
+
+
+  TaskRepo() {}
 
   late SharedPreferences sharedPreferences;
 
   Future<List<TaskModel>> getTaskList() async{
-    sharedPreferences = await SharedPreferences.getInstance();
-    final String jsonString = sharedPreferences.getString('task_list') ?? '[]';
+    //sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString =  await _readData();
     final List jsonDecoded = json.decode(jsonString) as List;
     return jsonDecoded.map((e) => TaskModel.fromJson(e)).toList();
   }
 
   void saveTaskList(List<TaskModel> tarefas) {
-    final String jsonString = json.encode(tarefas);
-    print(jsonString);
-    sharedPreferences.setString('task_list', jsonString);
+    _saveData(tarefas);
   }
+
+  Future<File> _getFile() async{
+
+    final directory = await getApplicationDocumentsDirectory();
+    final customDB = Directory("${directory.path}/taskDir");
+
+    if(!await customDB.exists()){
+      customDB.create(recursive: true);
+    }
+
+    return File("${customDB.path}/first.json");
+
+  }
+
+  Future<File> _saveData(List<TaskModel> save) async {
+     String data = json.encode(save);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async{
+    try{
+      final file = await _getFile();
+
+      if(await file.exists()) {
+        return file.readAsString();
+      }
+      else {
+        return "[]";
+      }
+    }
+    catch(e) {
+      return "[]";
+    }
+  }
+
+  // Future<void> _loadData() async{
+  //
+  //   String data = await _readData();
+  //
+  //
+  // }
+
+
 
 }
